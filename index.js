21e33ebf7faee347b7792a09093cfca4d4044b61
Syed Farjad Zia Zaidi <farjad.zaidi@sabaq.edu.pk>
2018-05-07 01:15:30 +0500

    Untracked Files

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header.js';
+
+const App = () =>
+    (
+        <Header textInputString={'Hey'} />
+        // <Text>Hey Whats Up Bitch</Text>
+    );
 
 AppRegistry.registerComponent('albums', () => App);
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+
+// AppRegistry.registerComponent('albums', () => App);
